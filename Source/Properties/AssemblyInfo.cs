342c61f5c04d5b0240de3a06fa0c400c2970f11c
DMagic <david.grandy@gmail.com>
2016-04-15 11:13:58 -0400

    version

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyInformationalVersion("v5.0")]
-[assembly: KSPAssembly("Science Params Modifier", 5, 0)]
+[assembly: AssemblyVersion("1.0.5.1")]
+[assembly: AssemblyFileVersion("1.0.5.1")]
+[assembly: AssemblyInformationalVersion("v5.1")]
+[assembly: KSPAssembly("Science Params Modifier", 5, 1)]
