60cce0cd1a154bf24f23376a2acf647ac6369c6c
DMagic <david.grandy@gmail.com>
2016-10-13 17:00:42 -0400

    New settings file, classes, and new game script

---
+++ Source/ScienceParamSettings.cs
@@ -0,0 +1,91 @@
+﻿#region license
+/*The MIT License (MIT)
+ScienceParamSettings - A persistent storage module for saving settings to disk
+Copyright (c) 2016 DMagic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using ScienceParamModifier.Framework;
+
+namespace ScienceParamModifier
+{
+	public class ScienceParamSettings : SM_ConfigNodeStorage
+	{
+		[Persistent]
+		public bool disableToolbar = false;
+		[Persistent]
+		public bool alterRecoveredData = false;
+		[Persistent]
+		public bool stockToolbar = true;
+
+		private smGameParameters settings;
+
+		private static ScienceParamSettings instance;
+
+		public static ScienceParamSettings Instance
+		{
+			get { return instance; }
+		}
+
+		public ScienceParamSettings(string file)
+		{
+			if (instance != null)
+				return;
+
+			instance = this;
+
+			FilePath = file;
+
+			GameEvents.OnGameSettingsApplied.Add(SettingsApplied);
+
+			if (Load())
+				LogFormatted("Settings file loaded");
+		}
+
+		public void SettingsApplied()
+		{
+			if (HighLogic.CurrentGame != null)
+				settings = HighLogic.CurrentGame.Parameters.CustomParams<smGameParameters>();
+
+			if (settings == null)
+				return;
+
+			if (settings.useAsDefault)
+			{
+				if (Save())
+					LogFormatted("Settings file saved");
+			}
+		}
+
+		public override void OnEncodeToConfigNode()
+		{
+			if (settings == null)
+				return;
+
+			disableToolbar = settings.disableToolbars;
+			alterRecoveredData = settings.editRecovered;
+			stockToolbar = settings.useStock;
+		}
+	}
+}
---
+++ Source/smGameParameters.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using ScienceParamModifier.Toolbar;
+
+namespace ScienceParamModifier
+{
+	public class smGameParameters : GameParameters.CustomParameterNode
+	{
+		private bool toolbar;
+
+		[GameParameters.CustomStringParameterUI("", lines = 2, autoPersistance = false)]
+		public string reload = "A re-load is required for some changes to take effect.";
+		[GameParameters.CustomParameterUI("Edit Recovered Data Value", autoPersistance = true)]
+		public bool editRecovered;
+		[GameParameters.CustomStringParameterUI("Warning", lines = 3)]
+		public string recoveryWarning = "The <b>Recovered Data Value</b> is used extensively by contracts and should not generally be altered.";
+		[GameParameters.CustomParameterUI("Disable All Toolbars", autoPersistance = true)]
+		public bool disableToolbars;
+		[GameParameters.CustomStringParameterUI("Warning", lines = 2)]
+		public string toolbarWarning = "This will only take effect if the <b>Use As Default</b> option is also selected";
+		[GameParameters.CustomParameterUI("Use Stock Toolbar", toolTip = "Switch between stock and Blizzy's toolbar", autoPersistance = true)]
+		public bool useStock = true;
+		[GameParameters.CustomParameterUI("Use As Default Values", toolTip = "Use the current settings as the defaults for all new games", autoPersistance = false)]
+		public bool useAsDefault;
+
+		public smGameParameters()
+		{
+			if (HighLogic.LoadedScene != GameScenes.MAINMENU)
+				return;
+
+			if (smConfigLoad.TopNode == null)
+				return;
+
+			editRecovered = smConfigLoad.Settings.alterRecoveredData;
+			disableToolbars = smConfigLoad.Settings.disableToolbar;
+			useStock = smConfigLoad.Settings.stockToolbar;
+
+			toolbar = ToolbarManager.ToolbarAvailable;
+		}
+
+		public override GameParameters.GameMode GameMode
+		{
+			get { return GameParameters.GameMode.CAREER | GameParameters.GameMode.SCIENCE; }
+		}
+
+		public override bool HasPresets
+		{
+			get { return false; }
+		}
+
+		public override string Section
+		{
+			get { return "DMagic Mods"; }
+		}
+
+		public override string Title
+		{
+			get { return "Science Param Modifier"; }
+		}
+
+		public override int SectionOrder
+		{
+			get { return 2; }
+		}
+
+		public override bool Enabled(System.Reflection.MemberInfo member, GameParameters parameters)
+		{
+			if (member.Name == "useStock")
+				return toolbar;
+			else if (member.Name == "reload")
+				return HighLogic.LoadedSceneIsGame;
+
+			return true;
+		}
+	}
+}
